70acb057e96e37387c58b456a11a59f5c0a23654
DEKEL USACH <48386383@ort>
2024-08-01 13:22:48 -0300

    commit 1

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Mi_Web_Personal_Individual.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
